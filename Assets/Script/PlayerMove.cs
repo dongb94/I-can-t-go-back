e110926a3dde55f49ee8eb902ff5882e37c768c2
Donggeon Byeon <dongb94@gmail.com>
2019-03-10 18:15:18 +0900

    Update PlayerMove

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,22 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private bool _isHandle;
+    private float _speed;
+
+    private enum Axis
     {
-        
+        Right,
+        Left,
+        Up,
+        Down
+    }
+    
+    private void Awake()
+    {
+        _isHandle = false;
+        _speed = 0.5f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (_isHandle) return;
+        
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
 
         if (horizontal != 0) transform.position += Vector3.right * horizontal;
         if (vertical != 0) transform.position += Vector3.up * vertical;
+    }
+
+    private void Move(Axis axis)
+    {
+        _isHandle = true;
+        switch (axis)
+        {
+            case Axis.Right:
+                transform.position += Vector3.right * _speed;
+                break;
+            case Axis.Left:
+                break;
+            case Axis.Up:
+                break;
+            case Axis.Down:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException("axis", axis, null);
+        }
     }
 }
