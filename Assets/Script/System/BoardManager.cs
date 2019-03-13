af9f1b6f7217ce3146c0f156cb083c5b1dd5f976
Donggeon Byeon <dongb94@gmail.com>
2019-03-14 00:42:26 +0900

    Update BoardManager

--- Assets/Script/System/BoardManager.cs
+++ Assets/Script/System/BoardManager.cs
@@ -11,9 +11,10 @@ public class BoardManager : Singleton<BoardManager>
     
     public enum TileColor
     {
-        Red = 1 << 10,
-        Blue = 1 << 11,
-        Green = 1 << 12
+        None = 1 << 10,
+        Red = 1 << 11,
+        Blue = 1 << 12,
+        Green = 1 << 13
     }
 
     public enum TileShape
@@ -25,22 +26,55 @@ public class BoardManager : Singleton<BoardManager>
         LowerRightTriangle = 1 << 4, // ┘
         LowerLeftTriangle = 1 << 5, // └
         
+        Hole = 1 << 6,
+        Laser = 1 << 7,
+        
+        Goal = 1 << 8,
+        
+        IsHollow = 1 << 9,
+        
+        HollowSquare = Square | IsHollow,
+        
+        HollowUpperRightTriangle = UpperRightTriangle | IsHollow,
+        HollowUpperLeftTriangle = UpperLeftTriangle | IsHollow,
+        HollowLowerRightTriangle = LowerRightTriangle | IsHollow,
+        HollowLowerLeftTriangle = LowerLeftTriangle | IsHollow,
     }
     protected override void Initialize()
     {
         base.Initialize();
         
-        Board = new int[BoardHeight][];
-        for (var i = 0; i < BoardHeight; i++)
+        Board = new int[BoardWight][];
+        for (var i = 0; i < BoardWight; i++)
         {
-            Board[i] = new int[BoardWight];
+            Board[i] = new int[BoardHeight];
         }
     }
+    
+    #region <Properties>
 
+    
+
+    #endregion
+
+    /// <summary>
+    /// Change Grid (x,y) to world position
+    /// (0,0) is bottom left
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
     public Vector3 ChangeGridToPosition(int x, int y)
     {
-        var xP = CellSize * x - CellSize / 2f;
-        var yP = CellSize * y - CellSize / 2f;
+        var xP = CellSize * (x - BoardWight / 2f) + CellSize / 2f;
+        var yP = CellSize * (y - BoardHeight/2) + CellSize / 2f;
+        
         return new Vector3(xP,yP,0);
     }
+
+    public void InsertTile(Tile tile)
+    {
+        Board[tile.x][tile.y] = (int)tile.color | (int)tile.shape;
+    }
+
 }
--- Assets/Script/Tiles/Tile.cs
+++ Assets/Script/Tiles/Tile.cs
@@ -6,6 +6,7 @@ public abstract class Tile : MonoBehaviour
 {
     [FormerlySerializedAs("Color")] public BoardManager.TileColor color;
     [FormerlySerializedAs("Shape")] public BoardManager.TileShape shape;
-    
-    
+
+    public int x;
+    public int y;
 }
