399d52efb45cd945c62c7aa1349d3ea063ffc5fa
Donggeon Byeon <dongb94@gmail.com>
2019-03-16 23:19:07 +0900

    Change tile info enum to tile

--- Assets/Script/System/BoardManager.cs
+++ Assets/Script/System/BoardManager.cs
@@ -8,38 +8,9 @@ public class BoardManager : Singleton<BoardManager>
     public int BoardWight;
 
     public int[][] Board;
-    
-    public enum TileColor
-    {
-        None = 1 << 10,
-        Red = 1 << 11,
-        Blue = 1 << 12,
-        Green = 1 << 13
-    }
 
-    public enum TileShape
-    {
-        Square = 1 << 1, 
-        
-        UpperRightTriangle = 1 << 2, // ┐
-        UpperLeftTriangle = 1 << 3, // ┌
-        LowerRightTriangle = 1 << 4, // ┘
-        LowerLeftTriangle = 1 << 5, // └
-        
-        Hole = 1 << 6,
-        Laser = 1 << 7,
-        
-        Goal = 1 << 8,
-        
-        IsHollow = 1 << 9,
-        
-        HollowSquare = Square | IsHollow,
-        
-        HollowUpperRightTriangle = UpperRightTriangle | IsHollow,
-        HollowUpperLeftTriangle = UpperLeftTriangle | IsHollow,
-        HollowLowerRightTriangle = LowerRightTriangle | IsHollow,
-        HollowLowerLeftTriangle = LowerLeftTriangle | IsHollow,
-    }
+    public object[] TileSet;
+    
     protected override void Initialize()
     {
         base.Initialize();
--- Assets/Script/Tiles/Tile.cs
+++ Assets/Script/Tiles/Tile.cs
@@ -4,12 +4,44 @@ using UnityEngine.Serialization;
 
 public abstract class Tile : MonoBehaviour
 {
-    [FormerlySerializedAs("Color")] public BoardManager.TileColor color;
-    [FormerlySerializedAs("Shape")] public BoardManager.TileShape shape;
+    [FormerlySerializedAs("Color")] public TileColor color;
+    [FormerlySerializedAs("Shape")] public TileShape shape;
 
     public int x;
     public int y;
 
+    public enum TileColor
+    {
+        None = 1 << 10,
+        Red = 1 << 11,
+        Blue = 1 << 12,
+        Green = 1 << 13
+    }
+
+    public enum TileShape
+    {
+        Square = 1 << 1, 
+        
+        UpperRightTriangle = 1 << 2, // ┐
+        UpperLeftTriangle = 1 << 3, // ┌
+        LowerRightTriangle = 1 << 4, // ┘
+        LowerLeftTriangle = 1 << 5, // └
+        
+        Hole = 1 << 6,
+        Laser = 1 << 7,
+        
+        Goal = 1 << 8,
+        
+        IsHollow = 1 << 9,
+        
+        HollowSquare = Square | IsHollow,
+        
+        HollowUpperRightTriangle = UpperRightTriangle | IsHollow,
+        HollowUpperLeftTriangle = UpperLeftTriangle | IsHollow,
+        HollowLowerRightTriangle = LowerRightTriangle | IsHollow,
+        HollowLowerLeftTriangle = LowerLeftTriangle | IsHollow,
+    }
+    
     public void Initialize()
     {
         BoardManager.GetInstance.InsertTile(this);
