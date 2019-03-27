cc6439eb568e54d706b67bb9ca9248116ca8d5e5
Donggeon Byeon <dongb94@gmail.com>
2019-03-27 22:47:41 +0900

    Update CommonEventArgs

--- Assets/Script/Common/CommonEventArgs.cs
+++ Assets/Script/Common/CommonEventArgs.cs
@@ -1,13 +1,16 @@
 
 using System;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class CommonEventArgs : EventArgs
 {
-    public GameObject object1;
-    public GameObject object2;
+    public Object object1;
+    public Object object2;
     public int intFactor1;
     public int intFactor2;
     public float floatFactor1;
     public float floatFactor2;
+    public Vector3 vector1;
+    public Vector3 vector2;
 }
