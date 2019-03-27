2ee42bd84035e54281ac80a892b6257d020d1205
Donggeon Byeon <dongb94@gmail.com>
2019-03-27 23:02:25 +0900

    Update player move coroutine to no capture

--- Assets/Script/System/PlayerManager.cs
+++ Assets/Script/System/PlayerManager.cs
@@ -84,14 +84,19 @@ public class PlayerManager : Singleton<PlayerManager>
         var currentPosition = transform.position;
         
         var moveCoroutine = CoroutineFactory.GetInstance.CreateCoroutine(1 / _speed);
+        moveCoroutine.EventArgs.vector1 = currentPosition;
+        moveCoroutine.EventArgs.intFactor1 = moveX;
+        moveCoroutine.EventArgs.intFactor2 = moveY;
         moveCoroutine.SetAction((args) =>
             {
-                var destination = BoardManager.GetInstance.ChangeGridToPosition(X + moveX, Y + moveY);
-                PlayerManager.GetInstance.transform.position = moveCoroutine.Change(currentPosition, destination);
+                var destination = BoardManager.GetInstance.ChangeGridToPosition(GetInstance.X + args.intFactor1,
+                                                                            GetInstance.Y + args.intFactor2);
+                var startPosition = args.vector1;
+                PlayerManager.GetInstance.transform.position = args.thisCoroutine.Change(startPosition, destination);
             })
             .SetExitAction((args) =>
             {
-                UpdatePosition(X+moveX, Y+moveY);
+                GetInstance.UpdatePosition(GetInstance.X+args.intFactor1, GetInstance.Y+args.intFactor2);
             })
             .SetTrigger();
 
