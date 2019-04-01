9003f9afa2cbbfc710e8e327de8dea199c0c6e51
Donggeon Byeon <dongb94@gmail.com>
2019-04-01 23:26:59 +0900

    Add Common Data Type

---
+++ Assets/Script/Common/CommonDataType.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Script.Common
+{
+    [Serializable]
+    public class Row
+    {
+        public int[] rowdata;
+    }
+
+    [Serializable]
+    public class Location2D
+    {
+        public int x;
+        public int y;
+    }
+}
