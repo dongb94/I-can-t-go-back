bde9c7b82ec0ea9162c67ef1ca3f787c8ee625c7
Donggeon Byeon <dongb94@gmail.com>
2019-04-01 23:32:32 +0900

    Add BoardSizeTest

---
+++ Assets/Script/Editor/NewTestScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+ 
+namespace Tests
+{
+    public class BoardSizeTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void BoardSizeTest1()
+        {
+            Assert.AreEqual(BoardManager.GetInstance.BoardWight, BoardManager.GetInstance.Board.Length);
+            Assert.AreEqual(BoardManager.GetInstance.BoardHeight, BoardManager.GetInstance.Board[0].Length);
+        }
+ 
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator BoardSizeTest2()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            
+            Assert.AreEqual(BoardManager.GetInstance.BoardWight, BoardManager.GetInstance.Board.Length);
+            Assert.AreEqual(BoardManager.GetInstance.BoardHeight, BoardManager.GetInstance.Board[0].Length);
+            
+            yield return null;
+        }
+    }
+}
