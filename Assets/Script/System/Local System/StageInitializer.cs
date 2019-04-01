b11b3b5b66fc1ba9738fba431466f1e30a6b7af5
Donggeon Byeon <dongb94@gmail.com>
2019-04-01 23:31:57 +0900

    Update StageInitializer

--- Assets/Script/System/Local System/StageInitializer.cs
+++ Assets/Script/System/Local System/StageInitializer.cs
@@ -1,10 +1,13 @@
 
+using Script.Common;
 using UnityEngine;
 
 public class StageInitializer : MonoBehaviour
 {
+    public Location2D playerPosition;
+    
     private void Awake()
     {
-        PlayerManager.GetInstance.InitializePosition(5,8);
+        PlayerManager.GetInstance.InitializePosition(playerPosition.x,playerPosition.y);
     }
 }
