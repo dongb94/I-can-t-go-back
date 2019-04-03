b782473875b3223aaf8f7614a32729467277f690
Donggeon Byeon <dongb94@gmail.com>
2019-04-04 00:05:53 +0900

    Add parser to MapFileReader

--- Assets/Script/System/MapFileReader.cs
+++ Assets/Script/System/MapFileReader.cs
@@ -1,4 +1,5 @@
 
+using System;
 using UnityEngine;
 
 public class MapFileReader
@@ -9,6 +10,30 @@ public class MapFileReader
         
         
     }
-    
+
+    /// <summary>
+    /// 00 00 00 00
+    /// Sh Co X  Y
+    /// </summary>
+    /// <param name="mapFile"></param>
+    private void Parser(string mapFile)
+    {
+        var length = mapFile.Length;
+
+        var mapInfo = mapFile.Split('|');
+
+        foreach (var element in mapInfo)
+        {
+            var shape = int.Parse(element.Substring(0, 2));
+            var color = int.Parse(element.Substring(2, 4));
+            var x = int.Parse(element.Substring(4, 6));
+            var y = int.Parse(element.Substring(6));
+
+            var tileShape = (Tile.TileShape) shape;
+            var tileColor = (Tile.TileColor) color;
+
+            TileManager.GetInstance.GetTile(x, y, tileShape, tileColor);
+        }
+    }
     
 }
