d789556888aaa89370f5211deae6f8e14cfa7ed6
Donggeon Byeon <dongb94@gmail.com>
2019-04-05 00:57:07 +0900

    Update MapfileReader to singleton

--- Assets/Script/System/MapFileReader.cs
+++ Assets/Script/System/MapFileReader.cs
@@ -2,13 +2,12 @@
 using System;
 using UnityEngine;
 
-public class MapFileReader
+public class MapFileReader : Singleton<MapFileReader>
 {
     public void ReadMapFile(string filePath)
     {
         var file = Resources.Load<TextAsset>(filePath);
-        
-        
+        Parser(file.text);
     }
 
     /// <summary>
@@ -25,8 +24,8 @@ public class MapFileReader
         foreach (var element in mapInfo)
         {
             var shape = int.Parse(element.Substring(0, 2));
-            var color = int.Parse(element.Substring(2, 4));
-            var x = int.Parse(element.Substring(4, 6));
+            var color = int.Parse(element.Substring(2, 2));
+            var x = int.Parse(element.Substring(4, 2));
             var y = int.Parse(element.Substring(6));
 
             var tileShape = (Tile.TileShape) shape;
