104de7c7af24bd3f97a72bad49f2bcdde8ba47a9
Donggeon Byeon <dongb94@gmail.com>
2019-04-05 00:58:41 +0900

    Add MapFileRead function to StageInitializer

--- Assets/Script/System/Local System/StageInitializer.cs
+++ Assets/Script/System/Local System/StageInitializer.cs
@@ -1,13 +1,25 @@
 
+using System;
 using Script.Common;
 using UnityEngine;
 
 public class StageInitializer : MonoBehaviour
 {
     public Location2D playerPosition;
+    public Stage stage;
+
+    public enum Stage
+    {
+        Stage1,
+        Stage2,
+        Stage3,
+        Stage4,
+        Stage5,
+    }
     
     private void Awake()
     {
         PlayerManager.GetInstance.InitializePosition(playerPosition.x,playerPosition.y);
+        MapFileReader.GetInstance.ReadMapFile("Stages/"+stage.ToString());
     }
 }
