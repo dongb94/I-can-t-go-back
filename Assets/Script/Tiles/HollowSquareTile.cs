ce4f3a6b47a9c7b26d317c72eb6f35f747ee198b
Donggeon Byeon <dongb94@gmail.com>
2019-04-09 01:02:23 +0900

    Add HollowSquareTile.cs and update scene

--- Assets/Script/Tiles/HollowSquareTile.cs
+++ Assets/Script/Tiles/HollowSquareTile.cs
@@ -1,8 +1,26 @@
 
+using UnityEngine;
+
 public class HollowSquareTile : Tile
 {
     public override void TileEffect(Axis axis)
     {
-        Destroy(this);
+        var coroutine = CoroutineFactory.GetInstance.CreateCoroutine(0.8f);            
+        coroutine.EventArgs.thisCoroutine = coroutine;
+        var transform1 = transform;
+        coroutine.EventArgs.object1 = transform1;
+        coroutine.EventArgs.vector1 = transform1.localScale;
+
+        
+        coroutine.SetAction(args =>
+        {
+            var trans = args.object1 as Transform;
+            trans.localScale = args.thisCoroutine.Change(args.vector1, Vector3.zero);
+        }).SetExitAction(args =>
+        {
+            var trans = args.object1 as Transform;
+            trans.localScale = Vector3.zero;
+        });
+        coroutine.SetTrigger();
     }
 }
