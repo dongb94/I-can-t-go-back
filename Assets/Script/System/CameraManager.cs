91ae9a5cc9f1469c355d34c8f651bfdae4d0ce61
Donggeon Byeon <dongb94@gmail.com>
2019-06-11 23:43:16 +0900

    Update camera Manager

--- Assets/Script/System/CameraManager.cs
+++ Assets/Script/System/CameraManager.cs
@@ -16,8 +16,13 @@ public class CameraManager : Singleton<CameraManager>
 
     public void InitializeCameraPosition()
     {
-        if(IsEditorMode) mainCamera.transform.position += Vector3.right * 5;
-
         mainCamera.orthographicSize = 10 + Math.Max(BoardManager.GetInstance.BoardWidth - 20, 0) * 0.5f;
+
+        if (IsEditorMode)
+        {
+            var cameraPosition = new Vector3(Screen.width / 2f + 100, Screen.height / 2f, -10);
+            mainCamera.transform.position += mainCamera.ScreenToWorldPoint(cameraPosition);
+        }
+
     }
 }
